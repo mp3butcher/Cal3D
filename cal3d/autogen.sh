440cbe902d7bc0dd59408be1b796dd02e38366a5
Loic Dachary <loic@senga.org>
2004-12-09 14:44:04 +0000

    gracefully handle autoconf-2.13 installed by default

--- cal3d/autogen.sh
+++ cal3d/autogen.sh
@@ -1,57 +1,119 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
+
+PROJECT=cal3d
+ACLOCAL_FLAGS=
+LT_FLAGS='--force'
+AC_FLAGS=
+AM_FLAGS='--add-missing --force-missing --copy'
+TEST='-d examples'
+
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
 
 DIE=0
 
-
-: ${AUTOCONF=autoconf}
-: ${AUTOHEADER=autoheader}
-: ${ACLOCAL=aclocal}
-: ${AUTOMAKE=automake}
-: ${LIBTOOLIZE=libtoolize}
-
-($AUTOHEADER --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: Missing \`autoheader'. The version of \`autoconf'"
-  echo "installed doesn't appear recent enough."
-  echo "Download the appropriate package for your distribution,"
-  echo "or get the source (2.13 or newer) from:"
-  echo "http://www.gnu.org/software/autoconf/autoconf.html"
-  DIE=1
-}
-
-($ACLOCAL --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: Missing \`aclocal'.  The version of \`automake'"
-  echo "installed doesn't appear recent enough."
-  echo "See http://www.gnu.org/software/automake"
-  echo "to get a recent version (1.4 or newer)"
-  DIE=1
-}
-
-($LIBTOOLIZE --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: Missing libtoolize"
-  DIE=1
-}
-
-if test "$DIE" -eq 1; then
-  exit 1
+have_libtool=false
+if libtool --version < /dev/null > /dev/null 2>&1 ; then
+	libtool_version=`libtoolize --version | sed 's/^[^0-9]*\([0-9].[0-9.]*\).*/\1/'`
+	case $libtool_version in
+		1.4*|1.5*)
+			have_libtool=true
+			;;
+	esac
+fi
+if $have_libtool ; then : ; else
+	echo "You need libtool >= 1.4 installed to compile $PROJECT."
+	DIE=1
 fi
 
-aclocalinclude="$ACLOCAL_FLAGS"
+autoconf=''
+for at in autoconf autoconf-2.53 autoconf-2.54 autoconf-2.55 autoconf-2.56 autoconf-2.57 autoconf-2.58 autoconf-2.59; do
+	if $at --version < /dev/null > /dev/null 2>&1 ; then
+		at_version=`$at --version | grep -i 'autoconf' | sed 's/^[^0-9]*\(.*\)/\1/'`
+		case $at_version in
+			2.5?) 
+				autoconf="$at"
+				;;
+		esac
+	fi
+done
+if test -z $autoconf ; then
+	echo "You need autoconf >= 2.53 installed to compile $PROJECT."
+	DIE=1
+fi
 
-echo "Running $LIBTOOLIZE ..."
-$LIBTOOLIZE --force --copy
+automake=''
+for am in automake automake-1.7; do
+	if $am --version < /dev/null > /dev/null 2>&1 ; then
+		am_version=`$am --version | grep 'automake (GNU automake)' | sed 's/^[^0-9]*\(.*\)/\1/'`
+		case $am_version in
+			1.7*) 
+				automake="$am"
+				;;
+		esac
+	fi
+done
+if test -z $automake ; then
+	echo "You need automake >= 1.7 installed to compile $PROJECT."
+	DIE=1
+fi
 
-echo "Running $ACLOCAL $aclocalinclude ..."
-$ACLOCAL $aclocalinclude
+aclocal=''
+for acl in aclocal aclocal-1.7; do
+	if $acl --version < /dev/null > /dev/null 2>&1 ; then
+		acl_version=`$acl --version | grep 'aclocal (GNU automake)' | sed 's/^[^0-9]*\(.*\)/\1/'`
+		case $acl_version in
+			1.7*) 
+				aclocal="$acl"
+				;;
+		esac
+	fi
+done
+if test -z $aclocal; then
+	echo "You need aclocal >= 1.7 installed to compile $PROJECT."
+	DIE=1
+fi
 
-echo "Running $AUTOHEADER..."
-$AUTOHEADER
+if test "$DIE" -eq 1; then
+	echo
+	echo "You can fetch and install latest GNU tools from your distribution"
+	echo "packages or from http://ftp.gnu.org/gnu/. Note that aclocal is"
+	echo "part of the automake software package."
+	echo
+	exit 1
+fi
 
-echo "Running $AUTOMAKE ..."
-$AUTOMAKE --add-missing --copy --force-missing
+test $TEST || {
+	echo "You must run this script in the top-level $PROJECT directory"
+	exit 1
+}
 
-echo "Running $AUTOCONF ..."
-$AUTOCONF
+
+echo "Running '$aclocal $ACLOCAL_FLAGS'"
+$aclocal $ACLOCAL_FLAGS || exit $?
+
+# optionally run autoheader
+autoheader --version < /dev/null > /dev/null 2>&1
+if test $? -eq 0; then
+	echo "Running 'autoheader'"
+	autoheader || exit $?
+fi
+
+echo "Running 'libtoolize $LT_FLAGS'"
+libtoolize $LT_FLAGS || exit $?
+
+echo "Running '$automake $AM_FLAGS'"
+$automake $AM_FLAGS || exit $?
+
+echo "Running 'autoconf $AC_FLAGS'"
+autoconf $AC_FLAGS || exit $?
+
+cd $ORIGDIR
+
+echo
+echo "Now run ./configure and make."
+echo
