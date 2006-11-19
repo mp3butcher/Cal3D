e8a44821fdddcbc8967520af450b548109cab984
James W. Walker <jamesw@frameforge3d.com>
2006-11-19 23:51:30 +0000

    Added methods setCoreSubmesh, getCoreSubmesh, getBlendVertex to CalCoreSubMorphTarget.  New subclass CalCoreSubMorphTargetDiffMap.

--- cal3d/src/cal3d/coresubmorphtarget.cpp
+++ cal3d/src/cal3d/coresubmorphtarget.cpp
@@ -14,7 +14,7 @@
 
 
 #include "cal3d/coresubmorphtarget.h"
-
+#include "cal3d/coresubmesh.h"
 
 /*****************************************************************************/
 /** Returns the blend vertex vector.
@@ -44,6 +44,33 @@ int CalCoreSubMorphTarget::getBlendVertexCount() const
   return m_vectorBlendVertex.size();
 }
 
+ /*****************************************************************************/
+/** Record the core submesh that this morph applies to.
+  *
+  * This function associates the sub morph target with a core submesh.
+  *
+  * @param The core submesh.
+  *****************************************************************************/
+
+void	CalCoreSubMorphTarget::setCoreSubmesh( CalCoreSubmesh* inCoreSubmesh )
+{
+	m_coreSubmesh = inCoreSubmesh;
+}
+
+
+ /*****************************************************************************/
+/** Returns the core submesh associated with this morph.
+  *
+  * This function returns a pointer to the core submesh.
+  *
+  * @return The number of blend vertices.
+  *****************************************************************************/
+
+CalCoreSubmesh*		CalCoreSubMorphTarget::getCoreSubmesh() const
+{
+	return m_coreSubmesh;
+}
+
 
  /*****************************************************************************/
 /** Reserves memory for the blend vertices.
@@ -61,11 +88,19 @@ int CalCoreSubMorphTarget::getBlendVertexCount() const
 
 bool CalCoreSubMorphTarget::reserve(int blendVertexCount)
 {
-  // reserve the space needed in all the vectors
-  m_vectorBlendVertex.reserve(blendVertexCount);
-  m_vectorBlendVertex.resize(blendVertexCount);
+	bool	success = true;
+	try
+	{
+		// reserve the space needed in all the vectors
+		m_vectorBlendVertex.reserve(blendVertexCount);
+		m_vectorBlendVertex.resize(blendVertexCount);
+	}
+	catch (...)
+	{
+		success = false;
+	}
 
-  return true;
+	return success;
 }
 
  /*****************************************************************************/
@@ -78,7 +113,7 @@ bool CalCoreSubMorphTarget::reserve(int blendVertexCount)
   *
   * @return One of the following values:
   *         \li \b true if successful
-  *         \li \b false if an error happend
+  *         \li \b false if an error happened
   *****************************************************************************/
 
 bool CalCoreSubMorphTarget::setBlendVertex(int blendVertexId, const BlendVertex& blendVertex)
@@ -88,6 +123,225 @@ bool CalCoreSubMorphTarget::setBlendVertex(int blendVertexId, const BlendVertex&
   m_vectorBlendVertex[blendVertexId] = blendVertex;
 
   return true;
+}
+
+ /*****************************************************************************/
+/** Returns one blend vertex.
+  *
+  * This function returns a blend vertex from this sub morph target.
+  *
+  * @param vertexId  The ID of the vertex.
+  * @param outVertex Receives the blend vertex.
+  *****************************************************************************/
+
+void	CalCoreSubMorphTarget::getBlendVertex( int vertexId, BlendVertex& outVertex ) const
+{
+	outVertex = m_vectorBlendVertex[ vertexId ];
+}
+
+#pragma mark -
+
+ /*****************************************************************************/
+/** Reserves memory for the blend vertices.
+ *
+ * This function reserves memory for the blend vertices
+ * of the difference map.
+ *
+ * @param blendVertexCount The number of blend vertices that
+ *                    this difference map should be able to hold.
+ *
+ * @return One of the following values:
+ *         \li \b true if successful
+ *         \li \b false if an error happened
+ *****************************************************************************/
+
+bool CalSharedDifferenceMap::reserve(int blendVertexCount)
+{
+	bool	success = true;
+	try
+	{
+		// reserve the space needed in all the vectors
+		m_vectorBlendVertex.reserve(blendVertexCount);
+		m_vectorVertexIndex.reserve(blendVertexCount);
+	}
+	catch (...)
+	{
+		success = false;
+	}
+
+	return success;
+}
+
+ /*****************************************************************************/
+/** Record one blend vertex.
+  *
+  * This function adds a blend vertex to a sparse array. Vertices must be added
+  * in increasing order of ID.
+  *
+  * @param vertexId  The ID of the vertex.
+  * @param vertex    A blend vertex.
+  * @return One of the following values:
+  *         \li \b true if successful
+  *         \li \b false if an error happened
+  *****************************************************************************/
+
+bool CalSharedDifferenceMap::appendBlendVertex(int vertexID,
+	const CalCoreSubMorphTarget::BlendVertex& vertex)
+{
+	if ( (vertexID < 0) or (m_vectorVertexIndex.size() >= m_vectorVertexIndex.capacity()) )
+	{
+		return false;
+	}
+	if ( (not m_vectorVertexIndex.empty()) and (m_vectorVertexIndex.back() >= vertexID) )
+	{
+		return false;
+	}
+	m_vectorVertexIndex.push_back( vertexID );
+	m_vectorBlendVertex.push_back( vertex );
+	return true;
+}
+
+ /*****************************************************************************/
+/** Retrieve one blend vertex.
+  *
+  * This function gets a blend vertex from the difference map if it is present.
+  *
+  * If we were supporting random access, we would look up the vertex ID using
+  * binary search.  But actually, the physique calls getBlendVertex
+  * sequentially, so we optimize for that access.
+  *
+  * @param vertexId  The ID of the vertex.
+  * @param outVertex A blend vertex.
+  * @return One of the following values:
+  *         \li \b true if successful
+  *         \li \b false if the vertex is not present in the difference map
+  *****************************************************************************/
+
+bool	CalSharedDifferenceMap::getBlendVertex( int vertexId,
+									CalCoreSubMorphTarget::BlendVertex& outVertex ) const
+{
+	// I assume that we do not call getBlendVertex until after we are done
+	// appending vertices, so at this point we can cache direct access values
+	// for the vectors.
+	if (m_vectorVertexIndexPtr == NULL)
+	{
+		m_vectorVertexIndexPtr = &m_vectorVertexIndex[0];
+		m_vectorVertexIndexSize = m_vectorVertexIndex.size();
+		m_vectorBlendVertexPtr = &m_vectorBlendVertex[0];
+	}
+	
+	// With sequential access, the normal situation will be that
+	// (m_NextIndex == m_vectorVertexIndexSize) or (vertexId <= m_vectorVertexIndex[ m_NextIndex ])
+	// and
+	// either m_NextIndex == 0 or vertexId > m_vectorVertexIndex[ m_NextIndex - 1 ].
+	// Make sure that condition holds.
+	while ( (m_NextIndex < m_vectorVertexIndexSize) and
+		(vertexId > m_vectorVertexIndexPtr[ m_NextIndex ]) )
+	{
+		m_NextIndex += 1;
+	}
+	while ( (m_NextIndex > 0) and (vertexId <= m_vectorVertexIndexPtr[ m_NextIndex - 1 ]) )
+	{
+		m_NextIndex -= 1;
+	}
+	
+	bool	didFind = false;
+	
+	if ( (m_NextIndex < m_vectorVertexIndexSize) and
+		(vertexId == m_vectorVertexIndexPtr[ m_NextIndex ] ) )
+	{
+		outVertex = m_vectorBlendVertexPtr[ m_NextIndex ];
+		didFind = true;
+		m_NextIndex += 1;
+	}
+	return didFind;
+}
+
+#pragma mark -
+
+CalCoreSubMorphTargetDiffMap::CalCoreSubMorphTargetDiffMap()
+	: m_diffMap( new CalSharedDifferenceMap )
+{
+}
+
+CalCoreSubMorphTargetDiffMap::CalCoreSubMorphTargetDiffMap( const CalCoreSubMorphTargetDiffMap& inOther )
+	: m_diffMap( inOther.m_diffMap )
+{
+}
+
+ /*****************************************************************************/
+/** Reserves memory for the blend vertices.
+ *
+ * This function reserves memory for the blend vertices
+ * of the difference map.
+ *
+ * @param blendVertexCount The number of blend vertices that
+ *                    this sub morph target should be able to hold.
+ *
+ * @return One of the following values:
+ *         \li \b true if successful
+ *         \li \b false if an error happened
+ *****************************************************************************/
+
+bool CalCoreSubMorphTargetDiffMap::reserve(int blendVertexCount)
+{
+	return m_diffMap->reserve( blendVertexCount ) &&
+		CalCoreSubMorphTarget::reserve( blendVertexCount );
+}
+
+
+ /*****************************************************************************/
+/** Record the core submesh that this morph applies to.
+  *
+  * This function associates the sub morph target with a core submesh and caches
+  * blend vertices using the difference map.
+  *
+  * @param The core submesh.
+  *****************************************************************************/
+
+void	CalCoreSubMorphTargetDiffMap::setCoreSubmesh( CalCoreSubmesh* inCoreSubmesh )
+{
+	CalCoreSubMorphTarget::setCoreSubmesh( inCoreSubmesh );
+	
+	// Cache the blend vertices computed from the core vertices and the
+	// difference map.
+	const std::vector<CalCoreSubmesh::Vertex>&	coreVerts(
+		inCoreSubmesh->getVectorVertex() );
+	const unsigned int kNumVerts = coreVerts.size();
+	CalCoreSubMorphTarget::reserve( kNumVerts );
+	unsigned int vertexId;
+	BlendVertex	theVert, offset;
+	for (vertexId = 0; vertexId < kNumVerts; ++vertexId)
+	{
+		theVert.position = coreVerts[vertexId].position;
+		theVert.normal = coreVerts[vertexId].normal;
+		if (m_diffMap->getBlendVertex( vertexId, offset ))
+		{
+			theVert.position += offset.position;
+			theVert.normal += offset.normal;
+		}
+		setBlendVertex( vertexId, theVert );
+	}
+}
+
+
+ /*****************************************************************************/
+/** Record one blend vertex.
+  *
+  * This function adds a blend vertex to the difference map. Vertices must be added
+  * in increasing order of ID.
+  *
+  * @param vertexId  The ID of the vertex.
+  * @param vertex    A blend vertex.
+  * @return One of the following values:
+  *         \li \b true if successful
+  *         \li \b false if an error happened
+  *****************************************************************************/
+
+bool CalCoreSubMorphTargetDiffMap::appendBlendVertex(int vertexId,
+	const CalCoreSubMorphTarget::BlendVertex& vertex)
+{
+	return m_diffMap->appendBlendVertex( vertexId, vertex );
 }
 
 //****************************************************************************//
--- cal3d/src/cal3d/coresubmorphtarget.h
+++ cal3d/src/cal3d/coresubmorphtarget.h
@@ -14,7 +14,9 @@
 
 #include "cal3d/global.h"
 #include "cal3d/vector.h"
-
+#include "cal3d/refcounted.h"
+#include "cal3d/refptr.h"
+class CalCoreSubmesh;
 
 class CAL3D_API CalCoreSubMorphTarget
 {
@@ -26,16 +28,72 @@ public:
   };
   
 public:
-  CalCoreSubMorphTarget()  { }
-  ~CalCoreSubMorphTarget() { }
+  CalCoreSubMorphTarget() : m_coreSubmesh( NULL ) { }
+  virtual ~CalCoreSubMorphTarget() { }
+  
+  virtual void	setCoreSubmesh( CalCoreSubmesh* inCoreSubmesh );
+  CalCoreSubmesh*		getCoreSubmesh() const;
 
   int getBlendVertexCount() const;
   std::vector<BlendVertex>& getVectorBlendVertex();
-  bool reserve(int blendVertexCount);
+  virtual bool reserve(int blendVertexCount);
   bool setBlendVertex(int vertexId, const BlendVertex& vertex);
 
+  void	getBlendVertex( int vertexId, BlendVertex& outVertex ) const;
+
 private:
-  std::vector<BlendVertex> m_vectorBlendVertex;
+			CalCoreSubMorphTarget( const CalCoreSubMorphTarget& inOther );	// unimp
+			
+	std::vector<BlendVertex>	m_vectorBlendVertex;
+	CalCoreSubmesh*				m_coreSubmesh;
 };
+
+// The difference map is reference counted because we can use the same difference
+// map on multiple actor heads.
+class CalSharedDifferenceMap : public cal3d::RefCounted
+{
+public:
+			CalSharedDifferenceMap() : m_NextIndex(0),
+					m_vectorVertexIndexSize(0),
+					m_vectorVertexIndexPtr(0),
+					m_vectorBlendVertexPtr(0) { }
+
+  bool reserve(int blendVertexCount);
+  bool appendBlendVertex(int vertexId, const CalCoreSubMorphTarget::BlendVertex& vertex);
+  
+  bool	getBlendVertex( int vertexId, CalCoreSubMorphTarget::BlendVertex& outVertex ) const;
+
+protected:
+			~CalSharedDifferenceMap() { }
+
+private:
+	std::vector<CalCoreSubMorphTarget::BlendVertex>		m_vectorBlendVertex;
+	std::vector<int>									m_vectorVertexIndex;
+	mutable	int											m_NextIndex;
+	
+	// performance optimization: direct access to vector size and arrays
+	mutable int											m_vectorVertexIndexSize;
+	mutable const int*									m_vectorVertexIndexPtr;
+	mutable const CalCoreSubMorphTarget::BlendVertex*	m_vectorBlendVertexPtr;
+};
+typedef cal3d::RefPtr<CalSharedDifferenceMap> CalSharedDifferenceMapPtr;
+
+
+class CalCoreSubMorphTargetDiffMap : public CalCoreSubMorphTarget
+{
+public:
+			CalCoreSubMorphTargetDiffMap();
+			CalCoreSubMorphTargetDiffMap( const CalCoreSubMorphTargetDiffMap& inOther );
+			~CalCoreSubMorphTargetDiffMap() { }
+
+	virtual bool reserve(int blendVertexCount);
+	virtual void	setCoreSubmesh( CalCoreSubmesh* inCoreSubmesh );
+
+	bool appendBlendVertex(int vertexId, const CalCoreSubMorphTarget::BlendVertex& vertex);
+
+private:
+	CalSharedDifferenceMapPtr	m_diffMap;
+};
+
 #endif
 //****************************************************************************//
