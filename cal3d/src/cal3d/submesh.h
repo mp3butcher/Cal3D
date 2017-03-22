70acb23678ebbb422ff496b051441559a3a64b1c
mp3butcher Win32 <mp3butcher@hotmail.com>
2017-03-22 19:53:55 +0100

    add my name:)

--- cal3d/src/cal3d/submesh.h
+++ cal3d/src/cal3d/submesh.h
@@ -1,6 +1,7 @@
 //****************************************************************************//
 // submesh.h                                                                  //
 // Copyright (C) 2001, 2002 Bruno 'Beosil' Heidelberger                       //
+// Copyright (C) 2012, 2017 Julien 'MpEbUtCh3r' Valentin                      //
 //****************************************************************************//
 // This library is free software; you can redistribute it and/or modify it    //
 // under the terms of the GNU Lesser General Public License as published by   //
