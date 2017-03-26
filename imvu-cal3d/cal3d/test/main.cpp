a91602ceaaacd50d8c70ab9497e3cd10217be24f
mp3butcher <julienvalentin51@gmail.com>
2017-03-26 13:21:11 +0200

    rebind imvu cal3d as submodule to https://github.com/imvu/cal3d

--- imvu-cal3d/cal3d/test/main.cpp
+++
@@ -1,7 +0,0 @@
-#define BOOST_AUTO_TEST_MAIN
-#include <boost/test/auto_unit_test.hpp>
-
-// I really shouldn't have to do this...  investigate.  (The .lib file
-// contains main(), but when we link against it, the error "entry
-// point must be defined" appears.
-#include <boost/test/impl/unit_test_main.ipp>
